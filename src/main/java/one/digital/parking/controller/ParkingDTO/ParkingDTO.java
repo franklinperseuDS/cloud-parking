82429934664236d0cf284d665e3a518cc7f53f59
perseu-at-237228232680 <69854802+franklinperseuDS@users.noreply.github.com>
2022-11-22 21:09:50 -0400

    add JsonInclude to remove nulls

--- src/main/java/one/digital/parking/controller/ParkingDTO/ParkingDTO.java
+++ src/main/java/one/digital/parking/controller/ParkingDTO/ParkingDTO.java
@@ -1,7 +1,10 @@
 package one.digital.parking.controller.ParkingDTO;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ParkingDTO {
 
     private String id;
