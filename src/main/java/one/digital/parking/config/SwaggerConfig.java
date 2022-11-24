4f308c6f94e1dc7e63872575b44dd2c61779196e
perseu-at-237228232680 <69854802+franklinperseuDS@users.noreply.github.com>
2022-11-23 22:02:06 -0400

    adicionando swagger

---
+++ src/main/java/one/digital/parking/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package one.digital.parking.config;
+
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Component
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+}
